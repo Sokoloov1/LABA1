b17abd8c6dc96d50b094dd06486defb6ade3fb47
Sokoloov1 <167451365+Sokoloov1@users.noreply.github.com>
2024-12-03 19:11:35 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -4,72 +4,65 @@
 
 using namespace std;
 
-// Структура для узла стека
 struct NodeS {
     int data;
     NodeS* next;
 };
 
-// Структура для стека
 struct Stack {
     NodeS* top;
 };
 
-// Функция добавления элемента в стек
 void pushStack(Stack& stack, int value) {
-    NodeS* newNode = new NodeS(); // Создаем новый узел
-    newNode->data = value; // Присваиваем ему значение
-    newNode->next = stack.top; // Новый узел указывает на текущую вершину
-    stack.top = newNode; // Новый узел становится вершиной
+    NodeS* newNode = new NodeS(); 
+    newNode->data = value;
+    newNode->next = stack.top;   // Новый узел указывает на текущую вершину
+    stack.top = newNode;         // Новый узел становится вершиной
 }
 
-// Функция удаления элемента из стека
 void popStack(Stack& stack) {
     if (stack.top == nullptr) {
-        cout << "Стек пуст" << endl; // Если стек пуст, выводим сообщение
+        cout << "The stack is empty" << endl;//Стек пуст
         return;
     }
-    NodeS* temp = stack.top; // Сохраняем текущую вершину
-    stack.top = stack.top->next; // Перемещаем вершину на следующий элемент
-    delete temp; // Удаляем предыдущую вершину
+    NodeS* temp = stack.top;
+    stack.top = stack.top->next;
+    delete temp;
 }
 
-// Функция вывода содержимого стека на экран
 void printStack(Stack& stack) {
     if (stack.top == nullptr) {
-        cout << "Стек пуст" << endl; // Если стек пуст, выводим сообщение
+        cout << "The stack is empty" << endl;
         return;
     }
-    NodeS* temp = stack.top; // Начинаем с вершины стека
-    cout << "Элементы стека: ";
+    NodeS* temp = stack.top;
+    cout << "Stack Elements: ";
     while (temp != nullptr) {
-        cout << temp->data << " "; // Выводим значение текущего узла
-        temp = temp->next; // Переходим к следующему узлу
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     cout << endl;
 }
 
-// Функция освобождения памяти, занимаемой стеком
 void freeStack(Stack& stack) {
     while (stack.top != nullptr) {
         popStack(stack); // Удаляем узлы и освобождаем память
     }
 }
 
-// Функция записи стека в файл
 void writeStackToFile(const Stack& stack, const string& filename) {
-    ofstream file(filename); // Открываем файл для записи
+    ofstream file(filename);
 
     if (!file.is_open()) {
-        cerr << "Ошибка при открытии файла для записи" << endl; // Если файл не открыт, выводим сообщение
+        cerr << "Error when opening a file for recording" << endl;
         return;
     }
 
-    NodeS* temp = stack.top; // Начинаем с вершины стека
+    NodeS* temp = stack.top;
     while (temp != nullptr) {
-        file << temp->data << endl; // Записываем значение текущего узла в файл
-        temp = temp->next; // Переходим к следующему узлу
+        file << temp->data << endl;
+        temp = temp->next;
     }
 
-    file.close(); // Закрываем файл
+    file.close();
 }
